33b21cf217e4472fea4332efb7aa4cf445c0f4ae
quatinhe <quatinhe@outlook.com>
2024-05-01 22:40:00 +0100

    função apply_cdf

--- src/histogram_eq.cpp
+++ src/histogram_eq.cpp
@@ -22,6 +22,14 @@ namespace cp {
         return clamp(static_cast<unsigned char>(255 * (cdf_val - cdf_min) / (1 - cdf_min)));
     }
 
+    //function to apply cdf
+    void apply_cdf(const unsigned char* image, const std::vector<float> &cdf, unsigned char* output_image, int size){
+        #pragma omp parallel for
+            for(int i =0; i < size; i++){
+                output_image[i] = correct_color(cdf[image[i]], cdf[0]);
+            }
+    }
+
     static void histogram_equalization(const int width, const int height,
                                 const float *input_image_data,
                                 float *output_image_data,
@@ -40,7 +48,7 @@ namespace cp {
         //auto finish = std::chrono::high_resolution_clock::now();
             //std::chrono::duration<double> elapsed = finish - start;
             //std::cout << "Converter float to Uchar time: " << elapsed.count() << " s\n";
-
+        //TODO: Fazer isto uma função
         #pragma omp parallel for collapse(2)
         for (int i = 0; i < height; i++) //converte para tons de cinza (Leva aproximadamente 0.05s para o lake.ppm)
             for (int j = 0; j < width; j++) {
@@ -50,6 +58,7 @@ namespace cp {
                 auto b = uchar_image[3 * idx + 2];
                 gray_image[idx] = static_cast<unsigned char>(0.21 * r + 0.71 * g + 0.07 * b);
             }
+        /** O Colapse neste contexto permite o OpenMP distribuir as iterações de ambos os loops pelas threads*/
 
 
         std::fill(histogram, histogram + HISTOGRAM_LENGTH, 0); //inicializa histograma
@@ -57,22 +66,30 @@ namespace cp {
         for (int i = 0; i < size; i++) //calcula histograma
             histogram[gray_image[i]]++; //verificar se é necessario usar atomic
 
-        cdf[0] = prob(histogram[0], size); //calcula cdf
+        std::vector<float> cdf_vector(HISTOGRAM_LENGTH, 0);//nova variavel para teste
+        cdf_vector[0] = prob(histogram[0], size); //calcula cdf
         for (int i = 1; i < HISTOGRAM_LENGTH; i++)
-            cdf[i] = cdf[i - 1] + prob(histogram[i], size); //nao paralelizavel
+            cdf_vector[i] = cdf_vector[i - 1] + prob(histogram[i], size); //nao paralelizavel
 
         auto cdf_min = cdf[0];
         #pragma omp parallel for reduction(min: cdf_min)
         for (int i = 1; i < HISTOGRAM_LENGTH; i++)
             cdf_min = std::min(cdf_min, cdf[i]);
+        /**Este reduction permite que cada thread faça a operação em paralelo numa cópia privada do 'cdf_min' e combina
+         * o resultado usando a operação min
+         */
 
-        #pragma omp parallel for
+  /**      #pragma omp parallel for
         for (int i = 0; i < size_channels; i++) //correção de cor
-            uchar_image[i] = correct_color(cdf[uchar_image[i]], cdf_min);
+            uchar_image[i] = correct_color(cdf[uchar_image[i]], cdf_min);*/
+        std::vector<unsigned char> output_image(size_channels);
+        apply_cdf(uchar_image.get(), cdf_vector, output_image.data(), size_channels);
 
         for (int i = 0; i < size_channels; i++) //converte uchar para float
             output_image_data[i] = static_cast<float>(uchar_image[i]) / 255.0f;
     }
+
+
 
     wbImage_t iterative_histogram_equalization(wbImage_t &input_image, int iterations) {
 
